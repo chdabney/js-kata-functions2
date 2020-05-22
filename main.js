9dfc4723074a2b33bfb9682de415d51225e4a051
Chad <chdabney42@gmail.com>
2020-05-22 18:32:29 -0400

    refactored to use add function rather than ++ to increment

--- main.js
+++ main.js
@@ -1,32 +1,36 @@
+//The first three weren't so bad and once I had one completed one it snowballed into completion of the next one. 
+
 //add function
 function add(x, y) {
     return x + y
 }
 
-console.log(add(2, 4))
+console.log('kata 1:', add(2, 4))
 
 //Multiplication function
 function multiply(x, y) {
     let result = x
-    for (let index = 1; index < y; index++) {
+    for (let index = 1; index < y; index = add(index, 1)) {
         result = add(result, x)
     }
     return result
 }
 
-console.log(multiply(6, 4))
+console.log('kata 2:', multiply(6, 4))
 
 //Power/Exponentiation
 function power(x, n) {
     result = x
-    for (let index = 1; index < n; index++) {
+    for (let index = 1; index < n; index = add(index, 1)) {
         result = multiply(result, x)
     }
     return result
 }
 
-console.log(power(2, 8))
-console.log(power(3, 4))
+console.log('kata 3:', power(2, 8))
+console.log('kata 3:', power(3, 4))
+
+
 
 //Factorial
 // Write a function named "factorial" which takes a single argument and returns the factorial of that value.
@@ -37,14 +41,34 @@ console.log(power(3, 4))
 //For example, calling factorial(4) should return a result of 24.
 
 function factorial(number) {
-    let result = number
-    for (let index = number - 1; index > 0; index--) {
+    let result = 1
+    for (let index = 1; index < add(number, 1); index = add(index, 1)) {
         result = multiply(result, index)
+
     }
     return result
 }
 
-console.log(factorial(5))
+console.log('kata 4:', factorial(4))
+// BREAKDOWN:
+//result starts at 1 and index starts at 1
+//the cycle starts so now when loop checks if index[1] is less than number + 1(5) it will multiply 1 * 1 updating result to 1
+//result is now 1 and index updates to 2
+//next time around we check if 2 is < number + 1(5)
+//returns true so now we multiply 1 * 2 to get 2
+//result is now 2 and index increments to 3
+// we loop again checking 3 < number + 1(5) returns true
+// we multiply 2 * 3 updating result to 6 and index becomes 4
+// on the final loop with check if 4 < number + 1(5)
+// returns true so we multiply 6 * 4 updating result to 24 and increment index to 5
+// we loop again and check if index[5] < number+1(5)
+// returns false and we break the loop
+// result will return as 24
+
+// this one was a doozy!
+
+
+
 
 //Fibonacci
 // Write a function named "Fibonacci" which takes an argument n and returns the nth Fibonacci number.
@@ -57,7 +81,14 @@ console.log(factorial(5))
 // So, the number in brackets is the 8th Fibonacci number.
 
 function fibonacci(n) {
+    if (n <= 1) return 1;
 
+    return add(n - 1, n - 2)
 }
 
-console.log(fibonacci(8))
+console.log('kata 5:', fibonacci(8))
+
+//used below link as a guide
+//https://medium.com/developers-writing/fibonacci-sequence-algorithm-in-javascript-b253dc7e320e
+
+// this one is beyond me at the moment so I will return to it at a later date.
